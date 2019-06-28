82e9abf5f1b2fbf161705cdcf16f617655c8911c
Max <cerbereon@gmail.com>
2019-06-28 13:05:32 +0300

    Added student model and table, and styled students index page

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,23 @@
+class StudentsController < ApplicationController
+
+  def index
+  	@students = Student.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+end
---
+++ app/models/student.rb
@@ -0,0 +1,10 @@
+class Student < ApplicationRecord
+  before_save {self.email = email.downcase}
+  validates :name, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+  			uniqueness: { case_sensitive: false },
+  			length: { maximum: 100 },
+  			format: { with: VALID_EMAIL_REGEX }
+end
