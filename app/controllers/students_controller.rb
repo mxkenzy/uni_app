dc9dded0e13eb503ee8e0aaf3b07455b3ef09a77
Max <cerbereon@gmail.com>
2019-06-30 11:11:21 +0300

    Added show, edit and update user functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,11 @@
 class StudentsController < ApplicationController
+before_action :set_student, only: [:edit, :update, :show]
 
   def index
   	@students = Student.all
+  end
+
+  def show
   end
 
   def new
@@ -14,22 +18,30 @@ class StudentsController < ApplicationController
   	  flash[:success] = "You have successfully signed up"
   	  redirect_to root_path
   	else
-  	  render 'new'
+  	  render "new"
   	end
   end
 
   def edit
-
   end
 
   def update
-
+  	if @student.update(student_params)
+  	  flash[:success] = "You have successfully edited your account"
+  	  redirect_to @student
+  	else
+  	  render "edit"
+  	end
   end
 
   private
 
   def student_params
   	params.require(:student).permit(:name, :email)
+  end
+
+  def set_student
+	@student = Student.find(params[:id])
   end
 
 end
